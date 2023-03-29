1804eaa2ededa9cf9599aed38c9da45dd896f18c
tomasfrankovic <tomko.franky@gmail.com>
2023-03-29 18:15:56 +0200

    adding dist subtree

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-// vue.config.js
-
-/**
- * @type {import('@vue/cli-service').ProjectOptions}
- */
-module.exports = {
-    outputDir : './dist',
-    publicPath: process.env.NODE_ENV === "production" ? "gh-pages" : "/",
-  }
