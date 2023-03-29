2db4410d1464b04c5cb84c84f9c33cb6ccd26f70
tomasfrankovic <tomko.franky@gmail.com>
2023-03-29 16:17:03 +0200

    init 3

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,6 @@
  * @type {import('@vue/cli-service').ProjectOptions}
  */
 module.exports = {
-    outputDir : './docs',
+    outputDir : './dist',
+    publicPath: process.env.NODE_ENV === "production" ? "gh-pages" : "/",
   }
